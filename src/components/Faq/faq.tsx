5268f1da68ac86606adf073ff717d4aac0a41b8e
Shreyas Paliwal <shreyaspaliwal77@gmail.com>
2026-09-08 23:40:38 +0530

    dates updated

--- src/components/Faq/faq.tsx
+++ src/components/Faq/faq.tsx
@@ -46,7 +46,7 @@ const FaqComponent: React.FC = () => {
     },
     {
       question: `Dates for the workshop?`,
-      answer: `Workshop on Web Development is on the 12th and 13th of September' 2026. The Data Structures and Algorithms workshop is on the 19th and 20th of September 202.`,
+      answer: `Workshop on Web Development is on the 12th and 13th of September' 2026. The Data Structures and Algorithms workshop is on the 19th and 20th of September 2026.`,
     },
     {
       question: `Are there any prerequisites for the workshop?`,
